a44a3c911db82296ce1f0e4b4248435d43a8a1d9
fcoalonso2199 <fcoalonso21@gmail.com>
2026-09-12 17:42:50 -0600

    okydoky

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -31,7 +31,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   async function fetchMembers() {
     try {
-      const response = await fetch("data/members.json");
+      const response = await fetch("scripts/members.js");
       if (!response.ok) {
         throw new Error("Network response was not ok");
       }

07dcaa6961284975f0ac63f232462e80fe504ebf
fcoalonso2199 <fcoalonso21@gmail.com>
2026-09-12 17:50:57 -0600

    auditone

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -31,7 +31,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   async function fetchMembers() {
     try {
-      const response = await fetch("scripts/members.js");
+      const response = await fetch("data/members.json");
       if (!response.ok) {
         throw new Error("Network response was not ok");
       }
